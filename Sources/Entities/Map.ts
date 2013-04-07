d2792661dc25ec382aed8c0bbbb4e2b1fd9f25db
Der_Meister <der@heavysoft.ru>
2013-04-07 20:51:30 +0800

    Connected jQuery. Load map from JSON.

--- Sources/Entities/Map.ts
+++ Sources/Entities/Map.ts
@@ -1,4 +1,5 @@
 /// <reference path="../Engine/Image.ts" />
+/// <reference path="../Libs/jquery.d.ts" />
 
 module Bombardier.Entities {
     import Engine = Bombardier.Engine;
@@ -34,12 +35,21 @@ module Bombardier.Entities {
 
             this._cells[3][3] = 1;
             this._cells[1][1] = 1;
+
+            this._cells = jQuery.parseJSON('[[1,0,1],[0,1,0],[1,0,1]]');
         }
 
         draw(context: CanvasRenderingContext2D) {
             for (var y = 0; y < Map.HEIGHT; ++y) {
                 for (var x = 0; x < Map.WIDTH; ++x) {
-                    this._mapTiles[this._cells[y][x]].draw(context, x * Map.TILE_SIZE, y * Map.TILE_SIZE);
+                    if (this._cells[y] == undefined) {
+                        continue;
+                    }
+
+                    var tile = this._mapTiles[this._cells[y][x]];
+                    if (tile != undefined) {
+                        tile.draw(context, x * Map.TILE_SIZE, y * Map.TILE_SIZE);
+                    }
                 }
             }
         }
