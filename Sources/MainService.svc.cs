684c9a6a13a0c5e0403e6d94b94f7b5c49dc83b2
Der_Meister <der@heavysoft.ru>
2013-04-10 08:50:24 +0800

    Added AJAX-Enabled WCF service.

---
+++ Sources/MainService.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Bombardier
+{
+    public struct Result
+    {
+        public int One { get; set; }
+        public string Two;
+    }
+
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class MainService
+    {
+        // To use HTTP GET, add [WebGet] attribute. (Default ResponseFormat is WebMessageFormat.Json)
+        // To create an operation that returns XML,
+        //     add [WebGet(ResponseFormat=WebMessageFormat.Xml)],
+        //     and include the following line in the operation body:
+        //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
+        [OperationContract]
+        public Result DoWork()
+        {
+            Result res = new Result();
+
+            res.One = 123;
+            res.Two = "Trololo";
+            
+            // Add your operation implementation here
+            return res;
+        }
+
+        // Add more operations here and mark them with [OperationContract]
+    }
+}
