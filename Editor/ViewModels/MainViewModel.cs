502963aef6d11899474f893cdd5d53a612d5dcac
Der_Meister <der@heavysoft.ru>
2013-05-04 17:48:25 +0800

    Changed buttons to toggle buttons.

--- Editor/ViewModels/MainViewModel.cs
+++ Editor/ViewModels/MainViewModel.cs
@@ -10,22 +10,44 @@ namespace Bombardier.Editor.ViewModels
 {
     class MainViewModel : ViewModelBase
     {
+        public MapViewModel MapVM { get; private set; }
+
         public ICommand FileNewCommand { get; private set; }
 
-        public MapViewModel MapVM { get; private set; }
+        public ICommand ChangeTool { get; private set; }
 
         public MainViewModel()
         {
             FileNewCommand = new DelegateCommand(FileNew_Executed);
+            ChangeTool = new DelegateCommand<string>(ChangeTool_Executed);
 
             MapVM = new MapViewModel(new Bombardier.Common.Map(0, 0));
             OnPropertyChanged("MapVM");
+        }
+
+        bool isClearChecked;
+        public bool IsClearChecked
+        {
+            get { return isClearChecked; }
+            set
+            {
+                if (value != isClearChecked)
+                {
+                    isClearChecked = value;
+                    OnPropertyChanged("IsClearChecked");
+                }
+            }
         }
 
         void FileNew_Executed()
         {
             MapVM = new MapViewModel(new Bombardier.Common.Map(10, 10));
             OnPropertyChanged("MapVM");
+        }
+
+        void ChangeTool_Executed(string tool)
+        {
+            IsClearChecked = true; // @@
         }
     }
 }
