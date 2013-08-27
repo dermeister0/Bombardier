8a1857f36ff1750a1bea80b3f079e5c0006be606
Der_Meister <der@heavysoft.ru>
2013-08-27 23:18:27 +0800

    Minification.

---
+++ Minify.cmd
@@ -0,0 +1,9 @@
+set root=%~dp0
+del %root%\Main\Bombardier.min.js
+
+type "%root%\Main\Engine\*.js" > "%root%\Main\Bombardier0.js"
+type "%root%\Main\Entities\*.js" > "%root%\Main\Bombardier1.js"
+type "%root%\Main\app.js" > "%root%\Main\Bombardier2.js"
+type "%root%\Main\Global.js" > "%root%\Main\Bombardier3.js"
+
+type "%root%\Main\Bombardier*.js" | "%root%\Tools\jsmin" > "%root%\Main\Bombardier.min.js"
