a5f503e3ecd2d26fdb11c29d1a7b751ad8c4a2e1
Der_Meister <der@heavysoft.ru>
2013-09-04 23:01:04 +0800

    Added DataContract and DataMember attributes for proper JSON serialization.

--- Common/MapObject.cs
+++ Common/MapObject.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,13 +9,17 @@ namespace Bombardier.Common
 {
     public enum MapObjectType { Start };
 
+    [DataContract]
     [Serializable]
     public class MapObject
     {
+        [DataMember]
         public int X { get; set; }
 
+        [DataMember]
         public int Y { get; set; }
 
+        [DataMember]
         public MapObjectType ObjectType { get; set; }
     }
 }
