1c25b3a5e65b64b65a0e2543c9146fd5407be39d
Der_Meister <der@heavysoft.ru>
2013-09-10 18:51:54 +0800

    Simplified minify script. Removed *.map-files from project.

--- Minify.cmd
+++ Minify.cmd
@@ -1,11 +1,2 @@
 set root=%~dp0
-del %root%\Main\Bombardier.min.js
-
-type "%root%\Main\Engine\*.js" > "%root%\Main\Bombardier0.js"
-type "%root%\Main\Entities\*.js" > "%root%\Main\Bombardier1.js"
-type "%root%\Main\app.js" > "%root%\Main\Bombardier2.js"
-type "%root%\Main\Global.js" > "%root%\Main\Bombardier3.js"
-
-type "%root%\Main\Bombardier*.js" | "%root%\Tools\jsmin" > "%root%\Main\Bombardier.min.js"
-
-del "%root%\Main\Bombardier?.js"
+type "%root%\Main\Engine\*.js", "%root%\Main\Entities\*.js", "%root%\Main\app.js", "%root%\Main\Global.js" | "%root%\Tools\jsmin" > "%root%\Main\Bombardier.min.js"
