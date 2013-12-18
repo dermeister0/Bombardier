34531c6beae0331c635978b6e93d91bbf8dd0dbc
Der_Meister <der@heavysoft.ru>
2013-12-18 23:42:23 +0800

    Introduced EditEventView.

---
+++ Editor/Views/Dialogs/EditEventView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bombardier.Editor.Views.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for EditEventView.xaml
+    /// </summary>
+    public partial class EditEventView : Window
+    {
+        public EditEventView()
+        {
+            InitializeComponent();
+        }
+    }
+}
